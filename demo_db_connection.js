67d1859a88a063292c464ea8122da7720af62223
David Dai <daiwei19971009@gmail.com>
2019-11-10 10:51:27 -0800

    for code review

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -15,11 +15,29 @@ connection.connect(function(err) {
     }
     console.log('Connected as id ' + connection.threadId);
 });
+
 connection.query('USE UCSDSocial;');
 
-connection.query('SELECT * FROM Events', db, function (error, results, fields) {
+/*connection.query('SELECT * FROM Events where EventID = 007;', db, function (error, results, fields) {
+    console.log(error);
+    console.log(results);
+});*/
+
+//create event
+//connection.query("insert into Events VALUES ('010','AWS RDS Test', '001dic', 'danlam01', '2019-11-11', '10:00:00', NULL, False, 'Code', 'https://github.com/Beacon-UCSD', NULL);");
+
+// this is pull the information by the id
+connection.query('SELECT * FROM Events where EventID = 010;', db, function (error, results, fields) {
     console.log(error);
     console.log(results);
 });
+
+connection.query("UPDATE Events SET Description = 'This is a test' where EventID = 010");
+
+connection.query('SELECT * FROM Events where EventID = 010;', db, function (error, results, fields) {
+    console.log(error);
+    console.log(results);
+});
+
 
 connection.end();
