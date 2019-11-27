33c2dc1aa63794d9b4cb0c968348bcc40393df26
David Dai <daiwei19971009@gmail.com>
2019-11-27 10:15:59 -0800

    Updated schema w/ forum & host info

--- beancon.sql
+++ beancon.sql
@@ -29,49 +29,53 @@ CREATE TABLE Events(
     EventID VARCHAR(50) NOT NULL,
     Eventname TEXT NOT NULL,
     tagID VARCHAR(20) NOT NULL,
-    Host VARCHAR(20) NOT NULL,
+    Hostname VARCHAR(20) NOT NULL,
+    Hostemail VARCHAR(50) NOT NULL,
     Startdate date NOT NULL,
     Enddate date,
     Private boolean NOT NULL,
     Description TEXT,
     FlyerURL TEXT,
     Attendees TEXT,
+    Forum TEXT,
     primary key (EventID)
 );
 
-insert into Events VALUEs ('000','Hard Hack', '001bbb,000zpp', 'korkmaz', '2020-02-02', NULL, false, 'Come out and build awesome projects over 24 hours using Hardware we provide (You can bring your own parts if you want just make sure they are not complete projects)! Participation is free and food will be provided!!', NULL, 'korkmaz');
-insert into Events VALUEs ('001','John Birthday', '000aaz', 'john21', '2019-11-02 17:00:00', '2019-11-02 23:00:00', True, 'Happy birthday to John!', 'www.facebook.com', NULL);
-insert into Events VALUEs ('002','Appfolio', '001aaa', 'KHesung', '2020-01-02 13:30:00', NULL, false, NULL, 'http://appfolio.com/', NULL);
-insert into Events VALUEs ('003','Spring Career', '000aab', 'khosla', '2020-02-15', NULL, false, 'Annual Career Fair in the Srping', NULL, NULL);
-insert into Events VALUEs ('004','National Coockie Day', '155qas', 'UCSD', '2020-12-04', NULL, false, NULL, NULL, NULL);
-insert into Events VALUEs ('005','Triple Delta', '000aaa,000aaz,002las,000aps', 'DDD', '2019-11-05', NULL, false, 'alsjd aodfuh diviwe ivebf oeefbiwend ifvn qofn sdionousb odbehf ovniwuhof odfivn 0fnjh nh iu dfjv i v h', NULL, NULL);
-insert into Events VALUEs ('006','OMNIA', '000aaa,000aaf,000aps', 'poiuq023', '2020-01-01', NULL, false, 'DANCE DANCE DANCE', NULL, NULL);
-insert into Events VALUES ('007','BEACON Daily Meeting', '001dic', 'dlam002', '2019-11-10 10:00:00', '2019-11-10 12:00:00', True, 'Code', 'https://github.com/Beacon-UCSD', NULL);
-insert into Events VALUES ('010','AWS RDS Test', '001dic', 'danlam01', '2019-11-11 10:00:00', '2019-11-17 10:00:00', False, 'Code', 'https://github.com/Beacon-UCSD', NULL);
+insert into Events VALUEs ('000','Hard Hack', '001bbb,000zpp', 'korkmaz', 'korkmaz@ucsd.edu', '2020-02-02', NULL, false, 'Come out and build awesome projects over 24 hours using Hardware we provide (You can bring your own parts if you want just make sure they are not complete projects)! Participation is free and food will be provided!!', NULL, 'korkmaz', NULL);
+insert into Events VALUEs ('001','John Birthday', '000aaz', 'john21', 'john21@ucsd.edu','2019-11-02 17:00:00', '2019-11-02 23:00:00', True, 'Happy birthday to John!', 'www.facebook.com', NULL, 'Good Morning, 2019-11-02 18:21:00');
+insert into Events VALUEs ('002','Appfolio', '001aaa', 'KHesung','k3hesung@ucsd.edu', '2020-01-02 13:30:00', NULL, false, NULL, 'http://appfolio.com/', NULL, 'The Event is going to start soon, 2020-01-02 09:21:00');
+insert into Events VALUEs ('003','Spring Career', '000aab', 'khosla', 'khsolaaaaaaa@ucsd.edu', '2020-02-15', NULL, false, 'Annual Career Fair in the Srping', NULL, NULL, NULL);
+insert into Events VALUEs ('004','National Coockie Day', '155qas', 'UCSD','ucsd@ucsd.edu', '2020-12-04', NULL, false, NULL, NULL, NULL, 'The Event is going to start soon, 2019-12-04 09:21:00; Do we get free coockies 2019-12-04 09:30:00');
+insert into Events VALUEs ('005','Triple Delta', '000aaa,000aaz,002las,000aps', 'DDD', 'tridelta@ucsd.edu','2019-11-05', NULL, false, 'alsjd aodfuh diviwe ivebf oeefbiwend ifvn qofn sdionousb odbehf ovniwuhof odfivn 0fnjh nh iu dfjv i v h', NULL, NULL, NULL);
+insert into Events VALUEs ('006','OMNIA', '000aaa,000aaf,000aps', 'poiuq023','poiuq023@ucsd.edu', '2020-01-01', NULL, false, 'DANCE DANCE DANCE', NULL, NULL, "No Change!!! 2020-01-01 00:00:00");
+insert into Events VALUES ('007','BEACON Daily Meeting', '001dic', 'dlam002', 'dlam002@ucsd.edu', '2019-11-10 10:00:00', '2019-11-10 12:00:00', True, 'Code', 'https://github.com/Beacon-UCSD', NULL, NULL);
+insert into Events VALUES ('010','AWS RDS Test', '001dic', 'danlam01', 'danlam01@ucsd.edu', '2019-11-11 10:00:00', '2019-11-17 10:00:00', False, 'Code', 'https://github.com/Beacon-UCSD', NULL, NULL);
 
 
 CREATE TABLE PastEvents(
     EventID VARCHAR(50) NOT NULL,
     Eventname TEXT NOT NULL,
     tagID VARCHAR(20) NOT NULL,
-    Host VARCHAR(20) NOT NULL,
+    Hostname VARCHAR(20) NOT NULL,
+    Hostemail VARCHAR(50) NOT NULL,
     Startdate date NOT NULL,
     Enddate date,
     Private boolean NOT NULL,
     Description TEXT,
     FlyerURL TEXT,
     Attendees TEXT,
+    Forum TEXT,
     primary key (EventID)
 );
 
-insert into PastEvents VALUES ('aaa','AWS Account Test', '001dic', 'danlam01', '2018-11-11 10:00:00', '2018-11-17 10:00:00', False, 'Code', 'https://github.com/Beacon-UCSD', NULL);
+insert into PastEvents VALUES ('aaa','AWS Account Test', '001dic', 'Dan Lam','dlam002@ucsd.edu', '2018-11-11 10:00:00', '2018-11-17 10:00:00', False, 'Code', 'https://github.com/Beacon-UCSD', NULL, NULL);
 
 CREATE TABLE Users(
     UserID VARCHAR(50) NOT NULL,
     GoogleUID VARCHAR(50) NOT NULL,
     Username VARCHAR(50) NOT NULL,
     Password VARCHAR(20) NOT NULL,
-    Email VARCHAR(20) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
     Phone VARCHAR(20),
     tagID VARCHAR(20) NOT NULL,
     College TEXT,
