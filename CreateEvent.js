65b2e1dca5a68d8343840b77c682f40c8275a129
David Dai <daiwei19971009@gmail.com>
2019-12-01 11:02:16 -0800

    Added default

--- CreateEvent.js
+++ CreateEvent.js
@@ -10,6 +10,7 @@ import TagButton from '../components/TagButton';
 
 import './CreateEvent.css';
 
+
 const AWS = require('aws-sdk');
 const ID = 'AKIAJ5OIQS2D43QAEFMQ';
 const SECRET = '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';
@@ -20,45 +21,37 @@ const s3 = new AWS.S3({
     secretAccessKey: SECRET
 });
 
+
 class CreateEvent extends Component {
 
     constructor(props){
         super(props);
 
         this.state = {
+            // Set to true to disable form from being able to be submitted and
+            // from being able to update component state.
+            disableForm: false,
+
             Tags: [],
             Eventname:'',
             Description:'',
-            startDate: new Date(),
-            endDate: new Date(),
+            startDate: new Date(Date.now()+3600000), // default start date = 1 hour from now
+            endDate: new Date(Date.now()+5400000), // default end date = 30 minutes after start time
             Private: false,
             Public: true,
             flyerURL: '',
-            Attendees: '',
-            selectedFile: "",
-            objectFile: {}
+            Attendees: ''
         };
 
+        this.handleCreateEvent = this.handleCreateEvent.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleStartDateChange = this.handleStartDateChange.bind(this);
         this.handleEndDateChange = this.handleEndDateChange.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.fileChangedHandler = this.fileChangedHandler.bind(this);
 
         console.log("Check Date object's toString method: " + this.state.endDate);
 
     }
-
-    fileChangedHandler = event => {
-        console.log(this.state.objectFile);
-        console.log(event.target.files[0]);
-        let uploadPic = event.target.files[0];
-
-        this.setState({
-            objectFile: uploadPic,
-            selectedFile: URL.createObjectURL(event.target.files[0])
-        });
-      }
 
     handleInputChange(evt){
         if (evt.target.name === "Private"){
@@ -87,26 +80,85 @@ class CreateEvent extends Component {
     handleStartDateChange = (date) => {
         console.log(date);
         this.setState({
-            startDate: new Date(+date),
-        })
-    } 
+            startDate: date
+        });
+        // Check if start date is after end date, if so update end date.
+        if (date > this.state.endDate) {
+            console.log("Changing end date");
+            // Set end date to be 5 minutes after start time.
+            this.setState({
+                endDate: new Date(date.getTime()+5*60000) // set end date 5 minutes after start date
+            });
+        }
+    }
     handleEndDateChange = (date) => {
         console.log('Input Date: ' + date);
 
         this.setState({
             endDate: new Date(+date),
-        })
+        });
         console.log(this.state.endDate);
     }
 
     handleCreateEvent(event){
+        // Prevent page from refresh
         event.preventDefault();
 
+        // Prevent form from being submitted when disabled.
+        if (this.state.formDisabled) {
+            return;
+        }
+
+        // Disable form.
+        this.setState({
+            formDisabled: true
+        });
+
+        // Remove whitespace at beginning and end of event name/description.
+        this.setState({
+            Eventname: this.state.Eventname.trim(),
+            Description: this.state.Description.trim()
+        });
+
+        // Check for missing required fields
+        var errors = [];
+        if (this.state.Eventname.length < 5) {
+            errors.push("Event name must be at least 5 characters long.");
+        }
+        if (this.state.Description.length < 15) {
+            errors.push("Event description must be at least 15 characters long.");
+        }
+        if (this.state.startDate < (Date.now()+300000)) {
+            errors.push("Event must start at least 5 minutes into the future.");
+        }
+        if ((this.state.endDate-this.state.startDate) < 300000) {
+            errors.push("Event must have a duration of at least 5 minutes.");
+        }
+        if (this.state.Tags.length <= 0) {
+            errors.push("At least one event tag is required.");
+        }
+        // Check if any errors
+        if (errors.length > 0) {
+            console.error("Error! Cannot post event due to the following errors:");
+            for (var i = 0; i < errors.length; i++) {
+                console.error("  - " + errors[i]);
+            }
+
+            // Re-enable form
+            this.setState({
+                formDisabled: false
+            });
+            return;
+        }
         var reader = new FileReader();
 
         reader.name = this.state.objectFile.name;
-
-        reader.onload = function(e) {
+        var location = "";
+        if(render.name === undefined || render.name === null){
+            location = "https://ucsdsocial.s3.amazonaws.com/Default.png";
+        }
+        else{
+            reader.onload = function(e) {
             var params = {
                 Bucket: BUCKET,
                 //This is a quick-fix (very bad) prefferably a unique string to the event
@@ -119,29 +171,42 @@ class CreateEvent extends Component {
             s3.upload(params, function(s3Err, data) {
                 if (s3Err) throw s3Err
                 console.log(`File uploaded successfully at ${data.Location}`);
-            });
-        };
+                });
+            };
+            reader.readAsArrayBuffer(this.state.objectFile);
     
-        reader.readAsArrayBuffer(this.state.objectFile);
-    
-        var location = "https://ucsdsocial.s3.amazonaws.com/" + this.state.objectFile.name;
+            location = "https://ucsdsocial.s3.amazonaws.com/" + this.state.objectFile.name;
+        }
+        
 
-        var body = {
+        var reqParams = {
             Tags: this.state.Tags,
             Eventname: this.state.Eventname,
             Host: "Me",
-            Startdate: this.state.startDate.toString(),
-            Enddate: this.state.endDate.toString(),
+            Startdate: this.state.startDate.getTime(),
+            Enddate: this.state.endDate.getTime(),
             Private: this.state.Private,
             Description: this.state.Description,
             FlyerURL: location,
-            Attendees: "",
+            Attendees: ""
         };
-
-        pfetch.jsonPost('/api/storeEvent', body);
-        // go to eventfeed page
-        this.props.history.push('/app/Eventfeed');
+        pfetch.jsonPost('/api/storeEvent', reqParams, (json) => {
+            if (!json.success) {
+                console.error("Error! Could not post event.");
+                if ('message' in json) {
+                    console.error(json.message);
+                }
+                // Re-enable form.
+                this.setState({
+                    formDisabled: false
+                });
+                return;
+            }
+            // Redirect to events feed page.
+            this.props.history.push('/app/Eventfeed');
+        });
     }
+
 
     addTag() {
         var tagToAdd = this.refs.tagInputField.value;
@@ -167,16 +232,8 @@ class CreateEvent extends Component {
         this.refs.tagInputField.value = "";
     }
 
-    openNav() {
-      document.getElementById("mySidenav").style.width = "166px";
-      document.getElementById("main").style.marginLeft = "166px";
-    }
 
     /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-    closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-    }
     /*
     Form currently handles:
         EventID: Handled in index.js
@@ -191,23 +248,18 @@ class CreateEvent extends Component {
     */
     render(){
         return(
-          <div>
-            <div id="mySidenav" className="sidenav">
-              <a href="javascript:void(0)" className="closebtn" onClick={this.closeNav}>&times;</a>
-              <Link to='/app/Profile'>Profile</Link>
-              <Link to='/app/Eventfeed'>Events</Link>
-              <Link to='#'>Notification</Link>
-              <Link to='#'>Logout</Link>
-            </div>
+        <div>
 
             <div id="main">
-              <button onClick={this.openNav}>Open</button>
-              <button onClick={this.closeNav}>Close</button>
-              <form className="eventForm" onSubmit={this.handleCreateEvent.bind(this)}>
-                <label className="eventName">
-                    Event Name:
-                    <input name="Eventname" type="text" value={this.state.Eventname}
-                        onChange={this.handleChange} />
+              <form className="eventForm" onSubmit={this.handleCreateEvent}>
+                <label>
+                    <input className="eventName" name="Eventname" type="text" value={this.state.Eventname}
+                        onChange={this.handleChange} placeholder="Event Name" disabled={this.state.formDisabled} />
+                </label>
+                <label>
+
+                    <input className="description" name="Description" type="text" value={this.state.Description}
+                        onChange={this.handleChange} placeholder="Event Description" disabled={this.state.formDisabled} />
                 </label>
                 <MuiPickersUtilsProvider
                     className='date-picker'
@@ -216,20 +268,25 @@ class CreateEvent extends Component {
                             name ='startDate'
                             label='Choose a start time'
                             value={this.state.startDate}
-                            onChange={this.handleStartDateChange}/>
+                            onChange={this.handleStartDateChange}
+                            disabled={this.state.formDisabled} />
                         <DateTimePicker
                             name='endDate'
                             label='Choose an end time'
                             value={this.state.endDate}
-                            onChange={this.handleEndDateChange} />
+                            onChange={this.handleEndDateChange}
+                            disabled={this.state.formDisabled} />
                 </MuiPickersUtilsProvider>
                 <br/>
                 <label>
-                    Tags:
-                    <input name="Tags" type="text" placeholder={"Type tag to add..."}
-                        ref='tagInputField' />
+                    <input className="tag" name="Tags" type="text" placeholder={"Type tag to add..."}
+                        ref='tagInputField' disabled={this.state.formDisabled} />
+                    <br/>
+                    <button className="tags" type='button'
+                        onClick={this.addTag.bind(this)}
+                        disabled={this.state.formDisabled}>Add Tag</button>
                 </label>
-                <button type='button' onClick={this.addTag.bind(this)}>Add Tag</button>
+
                 <div ref='eventTags'>
                     {this.state.Tags.map((tag, i) => (
                         <TagButton key={i} tag={tag} />
@@ -237,24 +294,16 @@ class CreateEvent extends Component {
                 </div>
                 <br/>
                 <label>
-                    Event Description:
-                    <input name="Description" type="text" value={this.state.Description}
-                        onChange={this.handleChange} />
+                        <input className="Private" name="Private" type="checkbox" checked={this.state.Private}
+                            onChange={this.handleInputChange} disabled={this.state.formDisabled} />
+                        Private
                 </label>
                 <label>
-                        <input name="Private" type="checkbox" checked={this.state.Private}
-                            onChange={this.handleInputChange} />
-                        Private
-                    </label>
-                    <label>
-                        <input name="Public" type="checkbox" checked={this.state.Public}
-                            onChange={this.handleInputChange} />
+                        <input className="Public" name="Public" type="checkbox" checked={this.state.Public}
+                            onChange={this.handleInputChange} disabled={this.state.formDisabled} />
                         Public
-                    </label>
-                    <input type="file" onChange={this.fileChangedHandler}/>
-                <input className="submit" type="submit" value="Submit" />
-                <h1>{this.state.objectFile.name}</h1>
-                <img id="testImg" src={this.state.selectedFile} width="150" height="150"/>
+                </label>
+                <input className="submit" type="submit" value="Submit" disabled={this.state.formDisabled} />
             </form>
             </div>
           </div>
